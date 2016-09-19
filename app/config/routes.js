e3cc3f98c8689e02cf6e783fec3bb2f8bbf9fdf7
Mike Magnani <mikemagnani5@gmail.com>
2016-09-19 16:55:41 -0400

    added a router.js

---
+++ app/config/routes.js
@@ -0,0 +1,8 @@
+// Inclue the React library
+var React = require('react');
+
+// Include the Router
+var Router = require('react-router');
+var Route = Router.Route;
+//  Include the IndexRoute (catch-all route)
+var IndexRoute  = Router.IndexRoute;
