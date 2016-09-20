f0d95e9e59fd7a1ee806f61ab8daf40178024361
Mike Magnani <mikemagnani5@gmail.com>
2016-09-20 16:05:27 -0400

    added routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,3 +6,13 @@ var Router = require('react-router');
 var Route = Router.Route;
 //  Include the IndexRoute (catch-all route)
 var IndexRoute  = Router.IndexRoute;
+
+var Main = require('../components/Main.js');
+
+
+
+module.exports = (
+    <Route path='/' component={Main}>
+
+    </Route>
+);
